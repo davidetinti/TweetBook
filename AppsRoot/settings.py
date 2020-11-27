607225a331460763af51ac1c8c6719332b92d83a
Davide Tinti <dati981026@gmail.com>
2020-11-27 20:35:32 +0100

    added os and django_heroku

--- AppsRoot/settings.py
+++ AppsRoot/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,3 +131,5 @@ INSTALLED_APPS += [
 EASY_MAPS_GOOGLE_KEY = 'AIzaSyATg_isuGSCHIlJamrxAXfkFDTYhIz7ytM'
 
 EASY_MAPS_CENTER = (-41.3, 32)
+
+django_heroku.settings(locals())
